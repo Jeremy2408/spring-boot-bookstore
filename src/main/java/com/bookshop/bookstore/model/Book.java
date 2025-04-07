196daed532009b8524068ce03444704c0d77d498
Jeremy2408 <jeremyo5726@gmail.com>
2025-04-07 16:18:16 +0100

    Initial Spring Boot Bookstore app with DB connection and Book entity

---
+++ src/main/java/com/bookshop/bookstore/model/Book.java
@@ -0,0 +1,25 @@
+package com.bookshop.bookstore.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String publisher;
+    private String category;
+    private String isbn;
+    private double price;
+    private int stock;
+    private String imageUrl;
+}
