11f009fe5a9f1aee44f8803cd6700de69342614e
Jeremy2408 <jeremyo5726@gmail.com>
2025-04-25 17:59:12 +0100

    Add admin user creation logic and SQL script for deleting admin user

--- src/main/java/com/bookshop/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookshop/bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package com.bookshop.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.bookshop.bookstore.model.User;
+import com.bookshop.bookstore.repository.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner createAdmin(UserRepository repo, PasswordEncoder encoder) {
+        return args -> {
+            if (repo.findByUsername("admin2").isEmpty()) {
+                User admin = User.builder()
+                .username("admin2")
+                .password(encoder.encode("admin123")) 
+                .role("ADMIN")
+                .shippingAddress("TU Dublin HQ")
+                .paymentMethod("Visa **** 1111")
+                .build();
+                repo.save(admin);
+                System.out.println(" Admin user created.");
+            }
+        };
+    }
 }
